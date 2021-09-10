f6871a21dc10d8ff902b99561bb087dc8372f4ee
Kyle Samuel <Ksamuel92@gmail.com>
2021-09-10 11:17:56 -0400

    commit tree 9272a9d32ca9285a9e79e090ee5126d0b7f4c579

--- index.js
+++ index.js
@@ -20,3 +20,68 @@ let allWagesFor = function () {
 
     return payable
 }
+
+const createEmployeeRecord = (array) => {
+    const newEmployeeRecord = {};
+    newEmployeeRecord['firstName'] = array[0];
+    newEmployeeRecord['familyName'] = array[1];
+    newEmployeeRecord['title'] = array[2];
+    newEmployeeRecord['payPerHour'] = array[3];
+    newEmployeeRecord['timeInEvents'] = [];
+    newEmployeeRecord['timeOutEvents'] =[];
+    return newEmployeeRecord
+}
+
+const createEmployeeRecords = (arrayOfArrays) => {
+   return arrayOfArrays.map(array => createEmployeeRecord(array))
+}
+
+function createTimeInEvent(dateStamp) {
+   const dateStampArray = dateStamp.split(' ');
+   const dateStampObject = {}
+    dateStampObject['type'] = "TimeIn"
+    dateStampObject['hour'] = parseInt(dateStampArray[1]);
+    dateStampObject['date'] = dateStampArray[0];
+    this.timeInEvents.push(dateStampObject)
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const dateStampArray = dateStamp.split(' ');
+    const dateStampObject = {}
+    dateStampObject['type'] = "TimeOut"
+    dateStampObject['hour'] = parseInt(dateStampArray[1]);
+    dateStampObject['date'] = dateStampArray[0];
+    this.timeOutEvents.push(dateStampObject)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+   const timeInEvent =  this['timeInEvents'].find(dateStamp => dateStamp['date'] === date)
+   const timeOutEvent = this['timeOutEvents'].find(dateStamp => dateStamp['date'] === date)
+
+   const hoursWorked = (timeOutEvent['hour']/100) - (timeInEvent['hour']/100)
+   return hoursWorked
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    const amountOwed = hoursWorked * this['payPerHour']
+    return amountOwed
+}
+
+function allWagesFor() {
+    const datesWorked = this['timeInEvents'].map(timeInEvent => timeInEvent['date'])
+    const wageArray = datesWorked.map(date => wagesEarnedOnDate.call(this, date) )
+    const allWages = wageArray.reduce((p,c) => p+c)
+    return allWages
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) =>  srcArray.find(employeeRecord => employeeRecord['firstName'] === firstName)
+
+function calculatePayroll(arrayOfEmployees){
+   const arrayOfWages = arrayOfEmployees.map(employeeRecord => allWagesFor.call(employeeRecord))
+   debugger
+   const totalPayroll = arrayOfWages.reduce((p,c) => p+c)
+   return totalPayroll
+}
